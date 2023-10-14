6c1f38fa74cb32dda11e8f45a0fae1d06fcd610b
fiammacrown <admin@ypakala.com>
2023-10-14 16:02:49 +0200

    Lab2

--- Abeslamidze_Web/Controllers/HomeController.cs
+++ Abeslamidze_Web/Controllers/HomeController.cs
@@ -1,32 +1,32 @@
-﻿using Abeslamidze_Web.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Abeslamidze_Web.Controllers
 {
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private List<ListDemo> _listDemo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            _listDemo = new List<ListDemo>{
+            new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+            new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+            new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
         }
 
         public IActionResult Index()
         {
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
+            ViewData["Text"] = "Лабораторная работа 2";
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
