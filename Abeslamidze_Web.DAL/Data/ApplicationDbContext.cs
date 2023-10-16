bdedb7c11baf0b4264b5b683aa094a4ee8bdd8b9
fiammacrown <admin@ypakala.com>
2023-10-16 22:09:11 +0200

    Lab4

---
+++ Abeslamidze_Web.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Abeslamidze_Web.DAL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Abeslamidze_Web.DAL.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options)
+		{
+		}
+
+		public DbSet<ApplicationUser> Users { get; set; }
+	}
+}
--- Abeslamidze_Web/Program.cs
+++ Abeslamidze_Web/Program.cs
@@ -1,4 +1,6 @@
-using Abeslamidze_Web.Data;
+using Abeslamidze_Web.DAL.Data;
+using Abeslamidze_Web.DAL.Entities;
+using Abeslamidze_Web.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,13 +10,41 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddRazorPages();
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => {
+	options.SignIn.RequireConfirmedAccount = false;
+	options.Password.RequireLowercase = false;
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequireUppercase = false;
+	options.Password.RequireDigit = false;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+// Configure application cookie options
+builder.Services.ConfigureApplicationCookie(options =>
+{
+	options.LoginPath = $"/Identity/Account/Login";
+	options.LogoutPath = $"/Identity/Account/Logout";
+	// Other cookie options can be configured here
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Seed the database
+using (var scope = app.Services.CreateScope())
+{
+	var services = scope.ServiceProvider;
+	var context = services.GetRequiredService<ApplicationDbContext>();
+	var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+	var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+	DbInitializer.Seed(context, userManager, roleManager).GetAwaiter().GetResult();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
